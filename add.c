67267cbb02eaed5cf5e95834533ac9e034529a94
Will <113240809+Anti1212@users.noreply.github.com>
2022-09-11 12:17:49 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main();
+{
+  int a,b;
+  clrscr();
+  printf("Enter the value of a:\n");
+  scanf("%d",&a);
+  printf("Enter the value of b:\n");
+  scanf("%d",&b);
+  printf("The sum of given numbers is:\n%d",a+b);
+}
