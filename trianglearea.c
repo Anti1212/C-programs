fcfaab9a6997c19421669519b15f9095aad8f8e3
Will <113240809+Anti1212@users.noreply.github.com>
2022-09-11 12:23:04 +0530

    Create trianglearea.c

---
+++ trianglearea.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main();
+{
+  int b,h;
+  printf("Enter the base of triangle:\n");
+  scanf("%d",&b);
+  printf("Enter the height of triangle:\n");
+  scanf("%d",h);
+  printf("Area of the triangle is:\n%d",0.5*b*h);
+  getch();
+}
