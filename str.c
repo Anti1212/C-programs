2ae46fda46279a80f7e0108be9bf54044ec12527
Will <113240809+Anti1212@users.noreply.github.com>
2022-09-18 18:00:39 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  char a,str[30];
+  clrscr();
+  printf("Enter your name :");
+  scanf(a);
+  printf("Please re-enter your name : ");
+  scanf(str);
+  pintf("Your name saved in character 'a' is :",a);
+  printf("Your name saved in string 'str' is :",str);
+  getch();
+}
+  
